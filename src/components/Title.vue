d0bcae296e836dcea92a321ccc9de8b9bbac56d2
Josue Diaz <jdiaz77711@gmail.com>
2019-08-27 09:32:10 +0100

    add title animation

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,11 +1,27 @@
 <style lang="scss" scoped>
 	.heading {
-		background-image: linear-gradient(to right, coral, tomato);
+		h1 {
+			background-image: linear-gradient(to right, coral, tomato);
+			color: transparent;
+			-webkit-background-clip: text;
+			display: inline-block;
+			letter-spacing: 2px;
+			transition: all 0.2s;
+
+			&:hover {
+				transform: skewY(2deg) skewX(15deg) scale(1.1);
+				text-shadow: 5px 10px 20px rgba(black, 0.2);
+			}
+		}
+	}
+
+	.center-text {
+		text-align: center;
 	}
 </style>
 
 <template>
-	<header class="heading">
+	<header class="heading center-text">
 		<h1 v-text="msg"></h1>
 	</header>
 </template>
